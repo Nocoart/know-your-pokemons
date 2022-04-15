255ea9b2283073246e467fdff5dd613f4c4bfb67
Nicolas Guerineau <nicolasguerineau@MacBook-Air.com>
2022-04-15 10:28:08 +0200

    fix : fetching better data

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,43 +1,58 @@
-import axios from "axios";
+import axios, { Canceler } from "axios";
 import { useState, useEffect } from "react";
 import Loader from "../../components/Loader/Loader";
 
 //styles
 import "./Home.scss";
 
-interface Pokemon {}
-
-const Home = () => {
-  const [pokemonList, setPokemonList] = useState<any>();
+export interface Pokemon {
+  entry_number: number;
+  pokemon_species: { name: string; url: string };
+  details: {};
+}
+const Home: React.FC = () => {
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchData = async () => {
+      let cancelToken: Canceler;
+
       try {
-        const dataArray = [];
-        for (let i = 1; i < 151; i++) {
-          const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`);
-          const { data } = response;
-          dataArray.push(data);
-        }
-        // const response = await axios.get("https://pokeapi.co/api/v2/pokemon?offset=151&limit=151");
-        // const { results } = response.data;
-        // const dataArray = [...results];
-        // dataArray.map(async (el: { name: string; url: string }, index: number) => {
-        //   const response = await axios.get(el.url);
-        //   dataArray[index].details = response;
-        // });
+        const response = await axios.get("https://pokeapi.co/api/v2/pokedex/2/", {
+          cancelToken: new axios.CancelToken((c) => (cancelToken = c)),
+        });
+
+        const dataArray: Pokemon[] = response.data.pokemon_entries;
         console.log(dataArray);
+
+        dataArray.forEach(async (el) => {
+          const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${el.entry_number}/`, {
+            cancelToken: new axios.CancelToken((c) => (cancelToken = c)),
+          });
+          dataArray[el.entry_number - 1].details = response.data;
+        });
+
         setPokemonList(dataArray);
         setIsLoading(false);
       } catch (error: any) {
         console.log(error.message);
       }
+
+      return () => cancelToken();
     };
 
     fetchData();
   }, []);
-  return isLoading ? <Loader /> : <div>this will be a pokedex</div>;
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <div>
+      {pokemonList.map((pokemon, index: number) => {
+        return <div key={index}>{pokemon.pokemon_species.name}</div>;
+      })}
+    </div>
+  );
 };
 
 export default Home;
