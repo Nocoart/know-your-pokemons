cd949d1101e7069dedb996da179a3e81ba3261ed
Nicolas Guerineau <nicolasguerineau@MacBook-Air.com>
2022-04-16 01:50:38 +0200

    type color added

--- src/components/PokeType/PokeType.tsx
+++ src/components/PokeType/PokeType.tsx
@@ -10,7 +10,72 @@ interface Props {
 }
 
 const PokeType: React.FC<Props> = ({ type }) => {
-  return <div className="type">{type.type.name}</div>;
+  const getBackground = (typeName: string): string => {
+    let color: string = "";
+    switch (typeName) {
+      case "grass":
+        color = "var(--grass)";
+        break;
+      case "fire":
+        color = "var(--fire)";
+        break;
+      case "water":
+        color = "var(--water)";
+        break;
+      case "electric":
+        color = "var(--electric)";
+        break;
+      case "flying":
+        color = "var(--flying)";
+        break;
+      case "ground":
+        color = "var(--ground)";
+        break;
+      case "rock":
+        color = "var(--rock)";
+        break;
+      case "psychic":
+        color = "var(--psychic)";
+        break;
+      case "dragon":
+        color = "var(--dragon)";
+        break;
+      case "steel":
+        color = "var(--steel)";
+        break;
+      case "dark":
+        color = "var(--dark)";
+        break;
+      case "ghost":
+        color = "var(--ghost)";
+        break;
+      case "fighting":
+        color = "var(--fighting)";
+        break;
+      case "ice":
+        color = "var(--ice)";
+        break;
+      case "poison":
+        color = "var(--poison)";
+        break;
+      case "normal":
+        color = "var(--normal)";
+        break;
+      case "bug":
+        color = "var(--bug)";
+        break;
+      case "fairy":
+        color = "var(--fairy)";
+        break;
+    }
+    return color;
+  };
+
+  return (
+    <div className="type" style={{ backgroundColor: getBackground(type.type.name) }}>
+      {type.type.name}
+    </div>
+  );
 };
 
 export default PokeType;
