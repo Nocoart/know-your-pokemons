cbfe7fadc1072748a70cfb70ef7b51f04b805457
Nicolas Guerineau <nicolasguerineau@MacBook-Air.com>
2022-04-18 14:52:11 +0200

    game logic improved

--- src/components/CatchGame/CatchGame.tsx
+++ src/components/CatchGame/CatchGame.tsx
@@ -12,7 +12,6 @@ const ash = require("../../assets/img/ash.png");
 const hpBar = require("../../assets/img/hp-bar.png");
 
 //interfaces
-type catchable = {};
 interface Props {
   currentPokemon: IPokemon;
   setCurrentPokemon: React.Dispatch<React.SetStateAction<IPokemon>>;
@@ -20,13 +19,14 @@ interface Props {
 
 const CatchGame: React.FC<Props> = ({ currentPokemon, setCurrentPokemon }) => {
   const [isCaught, setIsCaught] = useState(false);
-  const [isCatchable, setIsCatchable] = useState("false");
-  const [oponnentLvl, setOponnentLvl] = useState(0);
-  const [playerLvl, setPlayerLvl] = useState(0);
+  const [isCatchable, setIsCatchable] = useState(false);
+  const [guess, setGuess] = useState("");
 
   const { id } = currentPokemon;
   const pokeMinLvl = (id / 151) * 80;
   const pokeMaxLvl = (id * 110) / 151;
+  const [oponnentLvl, setOponnentLvl] = useState(0);
+  const [playerLvl, setPlayerLvl] = useState(0);
 
   useEffect(() => {
     if (checkIfAlreadyCaught(currentPokemon)) setIsCaught(true);
@@ -37,15 +37,26 @@ const CatchGame: React.FC<Props> = ({ currentPokemon, setCurrentPokemon }) => {
 
     //update player lvl
     const cookie = Cookies.get("caughtList");
+    let playerLvl: number = 0;
     if (typeof cookie === "string") {
       const caughtList = JSON.parse(cookie);
       const caugthArray = caughtList.split(";");
-      console.log(caugthArray);
-      setPlayerLvl(Math.floor(caugthArray.lenght / 151));
+      playerLvl = Math.floor((caugthArray.length / 151) * 100);
+      setPlayerLvl(playerLvl);
     }
+
+    //update catchability
+    if (playerLvl + 10 >= oponnentLvl) setIsCatchable(true);
   }, []);
 
-  const handleSuccess = () => {
+  const handleSuccess = (guess: string) => {
+    //check success
+    if (guess.toUpperCase() !== currentPokemon.name.toUpperCase()) {
+      window.alert("oh no you missed it");
+      setCurrentPokemon({} as IPokemon);
+      return;
+    }
+
     const cookie = Cookies.get("caughtList");
     if (typeof cookie === "string") {
       const caughtList = JSON.parse(cookie);
@@ -65,9 +76,12 @@ const CatchGame: React.FC<Props> = ({ currentPokemon, setCurrentPokemon }) => {
     <div className="catch-game-container">
       <div className="opponent-container">
         <div className="opponent-info">
-          <div className="opponent-info-bottom">
+          <div className="info-top">
             <div className="opponent-name">{isCaught ? currentPokemon.name : "?"}</div>
-            <div className="oppnonent-level">{oponnentLvl}</div>
+            <div className="oppnonent-level">
+              <span className="level">Lv </span>
+              {oponnentLvl}
+            </div>
           </div>
 
           <div className="opponent-info-bottom">
@@ -84,14 +98,27 @@ const CatchGame: React.FC<Props> = ({ currentPokemon, setCurrentPokemon }) => {
       <div className="player-container">
         <img src={ash} alt="" className="player-img" />
         <div className="player-info">
-          <div className="player-info-top">
+          <div className="info-top">
             <div className="player-name">Ash Ketchum </div>
-            <div className="player-level">{playerLvl}</div>
+            <div className="player-level">
+              <span className="level">Lv </span>
+              {playerLvl}
+            </div>
           </div>
-          <PlayerDialog isCaught={isCaught} handleSuccess={handleSuccess} currentPokemon={currentPokemon} />
+          <PlayerDialog
+            isCaught={isCaught}
+            handleSuccess={handleSuccess}
+            currentPokemon={currentPokemon}
+            isCatchable={isCatchable}
+          />
+          {isCatchable && !isCaught && (
+            <div className="input-section">
+              <input type="text" value={guess} onChange={(e) => setGuess(e.target.value)} autoFocus={true} />
+              <button onClick={() => handleSuccess(guess)}>catch</button>
+            </div>
+          )}
         </div>
       </div>
-      <button onClick={handleSuccess}>catch</button>
     </div>
   );
 };
--- src/components/PlayerDialog/PlayerDialog.tsx
+++ src/components/PlayerDialog/PlayerDialog.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Typed from "react-typed";
 
 //styles
 import "./PlayerDialog.scss";
@@ -7,12 +8,25 @@ import { IPokemon } from "pokeapi-typescript";
 //interfaces
 interface Props {
   isCaught: boolean;
-  handleSuccess: () => void;
+  handleSuccess: (guess: string) => void;
   currentPokemon: IPokemon;
+  isCatchable: boolean;
 }
 
-const PlayerDialog: React.FC<Props> = ({ isCaught, handleSuccess, currentPokemon }) => {
-  return <div className="player-dialog">{isCaught ? "" : ""}</div>;
+const PlayerDialog: React.FC<Props> = ({ isCaught, isCatchable, handleSuccess, currentPokemon }) => {
+  const generateDialog = (): string => {
+    if (isCaught) return "You already caught this one, try catching new ones to level up ";
+    if (isCatchable) return "Hey! you can catch this one, but what it's name again ?";
+    else return "Oh no, your level is too low for now, try catching the ones that comes before in the pokedex";
+  };
+
+  return (
+    <>
+      <div className="player-dialog">
+        <Typed strings={[generateDialog()]} />
+      </div>
+    </>
+  );
 };
 
 export default PlayerDialog;
