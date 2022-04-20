2635c5ef34143e24bd85593100d6a44f76b18b74
Nicolas Guerineau <nicolasguerineau@MacBook-Air.com>
2022-04-20 19:20:21 +0200

    typo

--- src/components/PlayerDialog/PlayerDialog.tsx
+++ src/components/PlayerDialog/PlayerDialog.tsx
@@ -13,7 +13,7 @@ interface Props {
 const PlayerDialog: React.FC<Props> = ({ isCaught, isCatchable }) => {
   const generateDialog = (): string => {
     if (isCaught) return "You already caught this one, try catching new ones to level up ";
-    else if (!isCatchable) return "Oh no, your level is too low for now, try catching the weakest ones";
+    else if (!isCatchable) return "Oh no, your level is too low for now, try catching the weaker ones";
     else if (isCatchable) return "Hey! you can catch this one, but what it's name again ?";
     else return "error has occured";
   };
